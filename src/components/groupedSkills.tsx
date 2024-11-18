bc4bc5e8fc638d802783636942b278dff5fc4b9a
abaydar <amandabaydar@gmail.com>
2024-11-18 18:20:39 -0500

    create reusable component for grouped skills

---
+++ src/components/groupedSkills.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { SvgIcon } from './svgIcon';
+
+interface GroupedSkillsProps {
+  groupName: string;
+  skills: { 
+      name: string; 
+      icon: string; 
+      alt: string; 
+    }[]; 
+  setIsExperienceHovered: (isExperienceHovered: boolean) => void;
+}
+
+const GroupedSkills: React.FC<GroupedSkillsProps> = ({ groupName, skills, setIsExperienceHovered }) => {
+  const capitalize = (s: string) => {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
+  return (
+    <>
+      <h3 className="text-2xl text-center font-bold mb-8">{capitalize(groupName)}</h3>
+      <ul className="flex flex-wrap gap-4 mb-8">
+        {skills.map((skill, index) => (
+          <li
+            key={index}
+            className="relative p-4 w-40 rounded-lg transition-all duration-300 group hover:scale-105 m-auto"
+            onMouseEnter={() => setIsExperienceHovered(true)}
+            onMouseLeave={() => setIsExperienceHovered(false)}
+          >
+            <div className="absolute inset-0 bg-white/20 rounded-lg backdrop-blur-sm opacity-0 group-hover:opacity-10 transition-opacity duration-300 pointer-events-none"></div>
+            <div className="absolute inset-0 rounded-lg shadow-md group-hover:shadow-2xl transition-shadow duration-300 pointer-events-none"></div>
+            <div className="relative flex flex-col items-center">
+              <SvgIcon name={skill.icon} alt={skill.alt} className="w-10 h-10 mb-2"/>
+              {skill.name}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GroupedSkills;
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { frontend, backend, languages, tools } from '../lib/data/skills';
-import { SvgIcon } from './svgIcon';
+import GroupedSkills from './groupedSkills';
 
 interface SkillsProps {
   setIsExperienceHovered: (isExperienceHovered: boolean) => void;
@@ -26,63 +26,9 @@ const Skills: React.FC<SkillsProps> = ({ setIsExperienceHovered }) => {
   return (
     <div id='skills' className="p-4 text-left target:pt-20 pt-20">
       <h2 className="text-3xl font-bold mb-4">Skills</h2>
-      <h3 className="text-2xl text-center font-bold mb-8">Languages</h3>
-      <ul className="flex flex-wrap gap-4 mb-8">
-        {skills.languages.map((language, index) => (
-          <li
-            key={index}
-            className="relative p-4 w-40 rounded-lg transition-all duration-300 group hover:scale-105 m-auto"
-            onMouseEnter={() => setIsExperienceHovered(true)}
-            onMouseLeave={() => setIsExperienceHovered(false)}
-          >
-            <div className="absolute inset-0 bg-white/20 rounded-lg backdrop-blur-sm opacity-0 group-hover:opacity-10 transition-opacity duration-300 pointer-events-none"></div>
-            <div className="absolute inset-0 rounded-lg shadow-md group-hover:shadow-2xl transition-shadow duration-300 pointer-events-none"></div>
-            <div className="relative z-10 flex flex-col items-center">
-              <SvgIcon name={language.icon} alt={language.alt} className="w-10 h-10 mb-2"/>
-              {language.name}
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h3 className="text-2xl text-center font-bold mb-8">Frontend</h3>
-      <ul className="flex flex-wrap gap-4 mb-8">
-        {skills.frontend.map((frontend, index) => (
-          <li key={index} className="relative p-4 w-40 rounded-lg transition-all duration-300 group hover:scale-105 m-auto">
-            <div className="absolute inset-0 bg-white/20 rounded-lg backdrop-blur-sm opacity-0 group-hover:opacity-10 transition-opacity duration-300 pointer-events-none"></div>
-            <div className="absolute inset-0 rounded-lg shadow-md group-hover:shadow-2xl transition-shadow duration-300 pointer-events-none"></div>
-            <div className="relative z-10 flex flex-col items-center">
-              <SvgIcon name={frontend.icon} alt={frontend.alt} className="w-10 h-10 mb-2"/>
-              {frontend.name}
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h3 className="text-2xl text-center font-bold mb-8">Backend</h3>
-      <ul className="flex flex-wrap gap-4 mb-8">
-        {skills.backend.map((backend, index) => (
-          <li key={index} className="relative p-4 w-40 rounded-lg transition-all duration-300 group hover:scale-105 m-auto">
-            <div className="absolute inset-0 bg-white/20 rounded-lg backdrop-blur-sm opacity-0 group-hover:opacity-10 transition-opacity duration-300 pointer-events-none"></div>
-            <div className="absolute inset-0 rounded-lg shadow-md group-hover:shadow-2xl transition-shadow duration-300 pointer-events-none"></div>
-            <div className="relative z-10 flex flex-col items-center">
-              <SvgIcon name={backend.icon} alt={backend.alt} className="w-10 h-10 mb-2"/>
-              {backend.name}
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h3 className="text-2xl text-center font-bold mb-8">Tools/Misc</h3>
-      <ul className="flex flex-wrap gap-4 mb-8">
-        {skills.tools.map((tool, index) => (
-          <li key={index} className="relative p-4 w-40 rounded-lg transition-all duration-300 group hover:scale-105 m-auto">
-            <div className="absolute inset-0 bg-white/20 rounded-lg backdrop-blur-sm opacity-0 group-hover:opacity-10 transition-opacity duration-300 pointer-events-none"></div>
-            <div className="absolute inset-0 rounded-lg shadow-md group-hover:shadow-2xl transition-shadow duration-300 pointer-events-none"></div>
-            <div className="relative z-10 flex flex-col items-center">
-              <SvgIcon name={tool.icon} alt={tool.alt} className="w-10 h-10 mb-2"/>
-              {tool.name}
-            </div>
-          </li>
-        ))}
-      </ul>
+      {Object.entries(skills).map(([skillName, skillItems]) => (
+        <GroupedSkills key={skillName} groupName={skillName} skills={skillItems} setIsExperienceHovered={setIsExperienceHovered} />
+      ))}
     </div>
   );
 };
