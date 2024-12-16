43fa6577efec5ad43c1aa3beec0ea09701bfb5c9
abaydar <amandabaydar@gmail.com>
2024-12-16 18:19:11 -0500

    remove unnecessary css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import CustomLink from './customLink';
 const Header = () => {
   return (
     <>
-      <header className="sticky body-font z-10 w-screen top-0 left-0 border-b-2 border-gray-300">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <header className="sticky top-0 z-10 w-screen bg-lightBlue">
+        <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             {navigationLinks.map((link) => 
               <CustomLink href={`#${link}`} className="mr-5 border-b-4 border-transparent text-customGreen hover:border-pink-500 active:text-pink-500">
