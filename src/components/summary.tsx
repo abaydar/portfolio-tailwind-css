9edc033bae363d4e50aef8296b66758229ab0dca
abaydar <amandabaydar@gmail.com>
2024-12-16 19:06:26 -0500

    update summary component

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -6,24 +6,18 @@ import linkedinIcon from '../assets/icons/linkedinIcon.svg';
 
 const Summary: React.FC = () => {
   return (
-    <div className="p-6 target:pt-20">
-      <h1 className="text-3xl font-bold mb-2">{summaryData.name}</h1>
-      <h2 className="text-2xl">
-        {summaryData.title}
-      </h2>
-      <h3 className="text-xl mb-4">
-        {summaryData.location}
-      </h3>
-      <p>
-      {summaryData.description}
-      </p>
-      <div className="mt-4">
+    <div className="h-[80vh] flex flex-col">
+      <div className="ml-auto">
         <CustomLink href="https://github.com/abaydar" target="_blank">
           <img src={githubIcon} alt="GitHub" className="w-8 h-8 inline-block mr-4"/>
         </CustomLink>
         <CustomLink href="https://www.linkedin.com/in/amanda-baydar/" target="_blank">
           <img src={linkedinIcon} alt="Linkedin" className="w-8 h-8 inline-block"/>
         </CustomLink>
+      </div>
+      <div className="w-1/3 my-auto">
+        <h1 className="text-3xl font-bold mb-2">{summaryData.greeting}</h1>
+        <p>{summaryData.description}</p>
       </div>
     </div>
   );
