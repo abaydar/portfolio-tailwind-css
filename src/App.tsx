535a471df12ca2388335d3d2c20faba6540007a0
abaydar <amandabaydar@gmail.com>
2024-12-20 16:02:57 -0500

    wave borders between components

--- src/App.tsx
+++ src/App.tsx
@@ -9,37 +9,64 @@ import AboutMe from './components/aboutMe';
 function App() {
   return (
     <div className="h-screen overflow-y-auto">
-      <Header/>
+      <Header />
 
-    <div className="relative w-screen bg-gradient-to-r from-gradientDarkBlue to-gradientLightBlue">
-      <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[url('/src/assets/images/circuitRight.png')] before:bg-no-repeat before:bg-cover before:bg-center before:opacity-50"/>
-      <div className="content-layout relative z-10">
-        <Summary />
+      <div className="relative w-screen bg-gradient-to-r from-gradientDarkBlue to-gradientLightBlue">
+        <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[url('/src/assets/images/circuitRight.png')] before:bg-no-repeat before:bg-cover before:bg-center before:opacity-50"></div>
+        <div className="content-layout relative z-10">
+          <Summary />
+        </div>
       </div>
-    </div>
 
-    <div className="w-screen bg-lightBlue text-darkBlue">
-    <div className="content-layout">
-      <Skills />
-    </div>
-    </div>
-
-    <div className="relative w-screen bg-gradient-to-r from-gradientDarkBlue to-gradientLightBlue">
-      <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[url('/src/assets/images/circuitLeft.png')] before:bg-no-repeat before:bg-cover before:bg-center before:opacity-50"/>
-      <div className="content-layout">
-        <Experience />
+      <div className="w-full overflow-hidden -mt-40 relative z-15">
+        <svg viewBox="0 0 1440 150" className='w-full h-auto -mb-1'>
+          <path
+            d="M0,96 C288,32 576,160 864,96 C1152,32 1440,64 1440,64 L1440,160 L0,160 Z"
+            className="fill-lightBlue"
+          ></path>
+        </svg>
+        <div className="w-screen bg-lightBlue text-darkBlue">
+          <div className="content-layout">
+            <Skills />
+          </div>
+        </div>
+        <div className="md:-mb-10 mb-0 bg-gradient-to-r from-gradientDarkBlue to-gradientLightBlue">
+          <svg viewBox="0 0 1440 150" className='w-full h-auto'>
+            <path
+              d="M0,96 C288,32 576,160 864,96 C1152,32 1440,64 1440,64 L1440,160 L0,160 Z"
+              className="fill-lightBlue"
+              transform="scale(1, -1) translate(0, -150)"
+            ></path>
+          </svg>
+        </div>
       </div>
-    </div>
 
-    <div className="w-screen bg-lightBlue text-darkBlue">
-      <div className="content-layout">
-        <AboutMe/>
+      <div className="relative w-screen bg-gradient-to-r from-gradientDarkBlue to-gradientLightBlue">
+        <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[url('/src/assets/images/circuitLeft.png')] before:bg-no-repeat before:bg-cover before:bg-center before:opacity-50"></div>
+        <div className="content-layout">
+          <Experience />
+        </div>
       </div>
-    </div>
 
-    <Contact/>
+      <div className="w-full overflow-hidden -mt-40 relative z-15">
+        <svg viewBox="0 0 1440 150" className='w-full h-auto -mb-1'>
+          <path
+            d="M0,96 C288,32 576,160 864,96 C1152,32 1440,64 1440,64 L1440,160 L0,160 Z"
+            className="fill-lightBlue"
+          ></path>
+        </svg>
+        <div className="w-screen bg-lightBlue text-darkBlue">
+          <div className="content-layout">
+            <AboutMe />
+          </div>
+        </div>
+      </div>
+
+      <Contact />
     </div>
   );
 }
+
+
 
 export default App;
