d9d06e6cdbc271fcb8c0a0511600eb4a893966e4
abaydar <amandabaydar@gmail.com>
2025-01-08 16:26:02 -0500

    add title

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -4,10 +4,13 @@ import { portfolioCards } from '../../lib/data/portfolioCards';
 
 const Portfolio: React.FC = () => {
   return (
-    <div className="flex flex-col p-4 justify-between md:flex-row space-y-4 md:space-y-0 mb-24">
-      {portfolioCards.map(card => (
-        <PortfolioCard key={card.id} {...card} />
-      ))}
+    <div className='content-layout'>
+      <h2 className="text-3xl text-left font-bold mb-8">Portfolio:</h2>
+      <div className="flex flex-col justify-between md:flex-row md:space-y-0 space-y-4 mb-40 md:mb-24">
+        {portfolioCards.map(card => (
+          <PortfolioCard key={card.id} {...card} />
+        ))}
+      </div>
     </div>
   );
 };
