6246130d7aa92acfdecd8b193b9b0210c4a4c565
abaydar <amandabaydar@gmail.com>
2025-01-08 16:19:11 -0500

    align cards and adjust positioning of elements

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -4,7 +4,7 @@ import { portfolioCards } from '../../lib/data/portfolioCards';
 
 const Portfolio: React.FC = () => {
   return (
-    <div className="portfolio-container">
+    <div className="flex flex-col p-4 justify-between md:flex-row space-y-4 md:space-y-0 mb-24">
       {portfolioCards.map(card => (
         <PortfolioCard key={card.id} {...card} />
       ))}
--- src/components/portfolio/portfolioCard.tsx
+++ src/components/portfolio/portfolioCard.tsx
@@ -9,20 +9,18 @@ interface PortfolioCardProps {
 
 const PortfolioCard: React.FC<PortfolioCardProps> = ({ title, description, imageUrl, projectUrl }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg text-darkBlue bg-lightBlue">
+    <div className="max-w-sm overflow-hidden shadow-lg shadow-black text-darkBlue bg-lightBlue px-12 py-8">
+      <div className="font-bold text-xl mb-2">{title}</div>
       <img className="w-full" src={imageUrl} alt={title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
+      <div className="py-4">
         <p className="text-base">{description}</p>
       </div>
-      <div className="px-6 pt-4 pb-2">
       <button
-        className="bg-customYellow text-xl font-bold rounded-full px-16 clickable-button"
+        className="bg-customYellow text-xl font-bold rounded-full px-8 clickable-button"
         onClick={() => window.open(projectUrl, '_blank', 'noopener,noreferrer')}
       >
           View Project
         </button>
-      </div>
     </div>
   );
 };
