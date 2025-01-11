623594973d144707f88524003ed8adf840452206
abaydar <amandabaydar@gmail.com>
2025-01-11 10:07:16 -0500

    align items in rown and center in tablet bp

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -6,7 +6,7 @@ const Portfolio: React.FC = () => {
   return (
     <div className='content-layout'>
       <h2 className="text-3xl text-left font-bold mb-8">Portfolio:</h2>
-      <div className="flex flex-col justify-between md:flex-row md:space-y-0 space-y-4 mb-40 md:mb-24">
+      <div className="flex flex-col justify-between items-center lg:flex-row lg:space-y-0 space-y-4 mb-40 lg:mb-24">
         {portfolioCards.map(card => (
           <PortfolioCard key={card.id} {...card} />
         ))}
