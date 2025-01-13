e2b9251b645f75975897c289804debb5375c2d42
abaydar <amandabaydar@gmail.com>
2025-01-13 17:16:13 -0500

    move images to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Experience from './components/experience';
+// import Experience from './components/experience';
 import Header from './components/header';
 import Skills from './components/skills';
 import Summary from './components/summary';
 import AboutMe from './components/aboutMe';
 import ContactFooter from './components/contactFooter';
-// import Portfolio from './components/portfolio';
+import Portfolio from './components/portfolio';
 
 function App() {
   return (
@@ -45,8 +45,8 @@ function App() {
       <div className="relative w-screen bg-gradient-to-r from-gradientDarkBlue to-gradientLightBlue">
         <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[url('/src/assets/images/circuitLeft.png')] before:bg-no-repeat before:bg-cover before:bg-center before:opacity-50 pointer-events-none"></div>
         <div className="content-layout relative z-10">
-          <Experience />
-          {/* <Portfolio /> */}
+          {/* <Experience /> */}
+          <Portfolio />
         </div>
       </div>
 
--- src/lib/data/portfolioCards.ts
+++ src/lib/data/portfolioCards.ts
@@ -11,14 +11,14 @@ export const portfolioCards: PortfolioCard[] = [
     id: 1,
     title: 'Tech Networking',
     description: 'An app to connect graduates entering the tech industry, offering a platform to share job-seeking experiences, discuss code, practice algorithms, and build connections.',
-    imageUrl: '/src/assets/images/tech-networking.jpg',
+    imageUrl: '/public/images/tech-networking.jpg',
     projectUrl: 'https://wizardly-yonath-16fb12.netlify.app/'
   },
   {
     id: 2,
     title: 'Books Journey',
     description: 'An app designed for users to add their recently read books and explore what others are reading.',
-    imageUrl: '/src/assets/images/books-journey.webp',
+    imageUrl: '/public/images/books-journey.webp',
     projectUrl: 'https://upbeat-mcclintock-08a2d1.netlify.app/'
   }
 ];
